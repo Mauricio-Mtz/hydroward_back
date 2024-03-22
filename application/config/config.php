33e68030edc0a395cd98dc5f4c44bb389da14c11
Mauricio Martínez <maurimtz07@gmail.com>
2024-03-22 13:55:38 -0600

    cambio de url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hydroward_back/';
+$config['base_url'] = 'http://localhost/HydroWardGod/hydroward_back/';
 
 /*
 |--------------------------------------------------------------------------
